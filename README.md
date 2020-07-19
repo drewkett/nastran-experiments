5e7919bcc921df6d1659e6f4967ac0cda35e63bb
Andrew <burkett.andrew@gmail.com>
2020-07-18 19:01:53 -0700

    Add README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Nastran Benchmarks
+
+```
+> test.sh
+
+Benchmark #1: calcmass shapes_500k_bulk.dat
+  Time (mean ± σ):      1.364 s ±  0.096 s    [User: 1.266 s, System: 0.095 s]
+  Range (min … max):    1.280 s …  1.587 s    10 runs
+ 
+Benchmark #1: python3 mass.py shapes_500k.dat
+  Time (mean ± σ):     45.769 s ±  0.772 s    [User: 45.957 s, System: 1.060 s]
+  Range (min … max):   44.662 s … 46.873 s    10 runs
+
+```
--- test.sh
+++ test.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-cargo install hyperfine
+cargo install --root . hyperfine
 
-cargo install --git https://github.com/drewkett/nastran-rs --branch mass nastran --bin calcmass
+cargo install --root . --git https://github.com/drewkett/nastran-rs --branch mass nastran --bin calcmass
 
-~/.cargo/bin/hyperfine --warmup 1 "~/.cargo/bin/calcmass shapes_500k_bulk.dat"
+PATH=./bin:$PATH
+
+hyperfine --warmup 1 "calcmass shapes_500k_bulk.dat"
 
 pip3 install pynastran
 
-~/.cargo/bin/hyperfine --warmup 1 "python3 mass.py shapes_500k.dat"
+hyperfine --warmup 1 "python3 mass.py shapes_500k.dat"
