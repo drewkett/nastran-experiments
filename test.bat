9fbb79d66e71ec0f241b4e13f6e622f78778aadf
Andrew <burkett.andrew@gmail.com>
2020-07-22 11:03:20 -0700

    Set output to quiet. Run less of pynastran

--- test.bat
+++ test.bat
@@ -1,15 +1,15 @@
-cargo install --root . hyperfine
+cargo install -q --root . hyperfine
 
-cargo install --root serial --git https://github.com/drewkett/nastran-rs --no-default-features nastran --bin calcmass
+cargo install -q --root serial --git https://github.com/drewkett/nastran-rs --no-default-features nastran --bin calcmass
 
 .\bin\hyperfine --warmup 1 ".\serial\bin\calcmass shapes_500k_bulk.dat"
 
-cargo install --root parallel --git https://github.com/drewkett/nastran-rs --features parallel nastran --bin calcmass
+cargo install -q --root parallel --git https://github.com/drewkett/nastran-rs --features parallel nastran --bin calcmass
 
 .\bin\hyperfine --warmup 1 ".\parallel\bin\calcmass shapes_500k_bulk.dat"
 
 python -m venv venv
 
-venv\scripts\pip install pynastran
+venv\scripts\pip install -q pynastran
 
-.\bin\hyperfine --warmup 1 ".\venv\scripts\python mass.py shapes_500k.dat"
+.\bin\hyperfine --warmup 1 -m 3 ".\venv\scripts\python mass.py shapes_500k.dat"
--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 
-cargo install --root . hyperfine
+cargo install -q --root . hyperfine
 
 PATH=./bin:$PATH
 
-cargo install --root serial --git https://github.com/drewkett/nastran-rs --no-default-features nastran --bin calcmass
+cargo install -q --root serial --git https://github.com/drewkett/nastran-rs --no-default-features nastran --bin calcmass
 
 hyperfine --warmup 1 "./serial/bin/calcmass shapes_500k_bulk.dat"
 
-cargo install --root parallel --git https://github.com/drewkett/nastran-rs --features parallel nastran --bin calcmass
+cargo install -q --root parallel --git https://github.com/drewkett/nastran-rs --features parallel nastran --bin calcmass
 
 hyperfine --warmup 1 "./parallel/bin/calcmass shapes_500k_bulk.dat"
 
@@ -17,6 +17,6 @@ python3 -m venv venv
 
 PATH=./venv/bin:$PATH
 
-pip install pynastran
+pip install -q pynastran
 
-hyperfine --warmup 1 "python mass.py shapes_500k.dat"
+hyperfine --warmup 1 -m 3 "python mass.py shapes_500k.dat"
